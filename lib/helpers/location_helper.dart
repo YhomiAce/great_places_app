c20e4c12fd80046bd36aefbe2efd83e163338235
YhomiAce <kareemyomi91@gmail.com>
2022-01-22 19:36:34 +0100

    google maps and selecting map area

--- lib/helpers/location_helper.dart
+++ lib/helpers/location_helper.dart
@@ -1,16 +1,13 @@
-const GOOGLE_API_KEY = 'AIzaSyC9iLh_Dblvix8Pd4dE7HX911Aj7dQGcJI';
-const GOOGLE_SIGN = 'B9u-ihi5TbRHEAeYtWAoCBTzeYU=';
+// const GOOGLE_API_KEY = 'AIzaSyDZ1HsNmYitBuUcwcFO4OcFXGGeQif6icM'; // Sadiq Key
+const GOOGLE_API_KEY = 'AIzaSyAObMzoszg93PuKpMW5JpUjbN4H9JC0isY'; // My Key
 
 class LocationHelper {
   static String generateLocationPreviewImage(
       {double latitude, double longitude}) {
-    return 'https://maps.googleapis.com/maps/api/staticmap?center=&$latitude,$longitude&zoom=10&size=400x400&maptype=roadmap&markers=color:red%7Clabel:C%7C$latitude,$longitude&key=$GOOGLE_API_KEY';
-    // return 'https://maps.googleapis.com/maps/api/staticmap?center=&37.4219983,-122.084&zoom=10&size=400x400&maptype=roadmap&markers=color:red%7Clabel:C%7C37.4219983,-122.084&key=AIzaSyDEh-6si30SqmSpZLQNZ_-Y7zksH71u93o&signature=B9u-ihi5TbRHEAeYtWAoCBTzeYU=&signature=BT6kWruq4pQjUFSWEK0FPNsLKuE=';
+    return 'https://maps.googleapis.com/maps/api/staticmap?center=&$latitude,$longitude&zoom=16&size=600x400&maptype=roadmap&markers=color:red%7Clabel:A%7C$latitude,$longitude&key=$GOOGLE_API_KEY';
+  }
+
+  static Future<String> getPlaceAddress(double lat, double long) async {
+    
   }
 }
-
-// signature=BASE64_SIGNATURE
-var signUrl =
-    'https://maps.googleapis.com/maps/api/staticmap?center=&37.4219983,-122.084&zoom=10&size=400x400&maptype=roadmap&markers=color:red%7Clabel:C%7C37.4219983,-122.084&key=AIzaSyDEh-6si30SqmSpZLQNZ_-Y7zksH71u93o&signature=B9u-ihi5TbRHEAeYtWAoCBTzeYU=&signature=BT6kWruq4pQjUFSWEK0FPNsLKuE=';
-var unsignUrl =
-    'https://maps.googleapis.com/maps/api/staticmap?center=&37.4219983,-122.084&zoom=10&size=400x400&maptype=roadmap&markers=color:red%7Clabel:C%7C37.4219983,-122.084&key=AIzaSyDEh-6si30SqmSpZLQNZ_-Y7zksH71u93o&signature=B9u-ihi5TbRHEAeYtWAoCBTzeYU=';
---
+++ lib/pages/map_screen.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+import '../models/place.dart';
+
+class MapScreen extends StatefulWidget {
+  final PlaceLocation initialLoctaion;
+  final bool isSelecting;
+
+  MapScreen({
+    this.initialLoctaion = const PlaceLocation(
+      latitude: 5.61460239763,
+      longitude: 4.7082553892,
+    ),
+    this.isSelecting = false,
+  });
+
+  @override
+  _MapScreenState createState() => _MapScreenState();
+}
+
+class _MapScreenState extends State<MapScreen> {
+  LatLng _pickedLocation;
+
+  void _selectLocation(LatLng position) {
+    setState(() {
+      _pickedLocation = position;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Area Map'),
+        actions: [
+          if (widget.isSelecting)
+            IconButton(
+              icon: Icon(Icons.check),
+              onPressed: _pickedLocation == null ? null : () {
+                Navigator.of(context).pop(_pickedLocation);
+              },
+            ),
+        ],
+      ),
+      body: GoogleMap(
+        initialCameraPosition: CameraPosition(
+          target: LatLng(
+            widget.initialLoctaion.latitude,
+            widget.initialLoctaion.longitude,
+          ),
+          zoom: 16,
+        ),
+        onTap: widget.isSelecting ? _selectLocation : null,
+        markers: _pickedLocation == null
+            ? {}
+            : {
+                Marker(markerId: MarkerId('m1'), position: _pickedLocation),
+              },
+      ),
+    );
+  }
+}
